4c05a85f9634e9fed1acdb763bb2a7d29f783c90
eborlee <eborlee@163.com>
2022-11-06 15:02:13 -0500

    Use SpringCache to store setmeal data

--- src/main/java/com/ebor/controller/SetmealController.java
+++ src/main/java/com/ebor/controller/SetmealController.java
@@ -13,6 +13,8 @@ import com.ebor.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,6 +35,7 @@ public class SetmealController {
     private CategoryService categoryService;
 
     @PostMapping
+    @CacheEvict(value="setmealCache", allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info(String.valueOf(setmealDto));
         setmealService.saveWithDish(setmealDto);
@@ -78,6 +81,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value="setmealCache", allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         log.info(ids.toString());
         setmealService.removeWithDish(ids);
@@ -85,6 +89,7 @@ public class SetmealController {
     }
 
     @GetMapping("/list")
+    @Cacheable(value="setmealCache", key = "#setmeal.categoryId +'_'+#setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> qw = new LambdaQueryWrapper<>();
         qw.eq(setmeal.getCategoryId()!=null, Setmeal::getCategoryId, setmeal.getCategoryId());
